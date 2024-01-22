96865c9513356bda44098b72b79c1dfd068f5142
Devkant <devkant21@gmail.com>
2024-01-22 16:54:43 +0530

    fix: Second.json

---
+++ contracts/first.sol
@@ -0,0 +1,8 @@
+pragma solidity >=0.7.0 <0.9.0;
+contract First { 
+        int public mydata; 
+        function GetDouble(int _data) public returns (int _output) { 
+                   mydata = _data * 2; 
+                   return mydata; 
+         } 
+     } 
---
+++ contracts/second.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.7.0 <0.9.0;
+import "./first.sol"; 
+         contract Second { 
+                address firstAddress;  
+                int public _data; 
+               constructor(address _first) { 
+                   firstAddress = _first; 
+               } 
+              function SetData() public { 
+                 First h = First(firstAddress); 
+                _data = h.GetDouble(21); 
+             } 
+       } 
